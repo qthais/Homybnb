a8779bcc005cfbe2b83431cba25179c336086149
qthais <thaiallb63@gmail.com>
2025-03-17 22:42:24 +0700

    remove redundant filter, fail to test

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -3,7 +3,6 @@ import { AuthModule } from './auth.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AUTH } from '@app/common';
-import { GrpcExceptionInterceptor } from './interceptors/grpc-exception.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
@@ -13,7 +12,6 @@ async function bootstrap() {
       protoPath: join(__dirname, '../auth.proto'),
     },
   });
-  app.useGlobalInterceptors(new GrpcExceptionInterceptor());
   await app.listen()
   
 }
