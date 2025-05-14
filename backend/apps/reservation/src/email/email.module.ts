2cc475bba4bf133a4ea37b7906ffc393688fad5d
qthais <thaiallb63@gmail.com>
2025-05-14 22:24:03 +0700

    fix(email): email folder position

---
+++ backend/apps/reservation/src/email/email.module.ts
@@ -0,0 +1,37 @@
+import { Global, Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import {MailerModule} from '@nestjs-modules/mailer'
+import { ConfigService } from '@nestjs/config';
+import { resolve } from 'path';
+import {EjsAdapter} from '@nestjs-modules/mailer/dist/adapters/ejs.adapter'
+@Global()
+@Module({
+  imports:[
+    MailerModule.forRootAsync({
+      useFactory:async(config:ConfigService)=>({
+        transport:{
+          host:config.get('SMTP_HOST'),
+          secure:true,
+          auth:{
+            user:config.get('SMTP_MAIL'),
+            pass:config.get('SMTP_PASSWORD'),
+          }
+        },
+        defaults:{
+          from: 'Qtdemy'
+        },
+        template:{
+          dir:resolve(process.cwd(), 'libs/common/src/email-templates'),
+          adapter:new EjsAdapter(),
+          options:{
+            strict:false
+          },
+        },
+      }),
+      inject:[ConfigService]
+    })
+  ],
+  providers: [EmailService],
+  exports:[EmailService]
+})
+export class EmailModule {}
